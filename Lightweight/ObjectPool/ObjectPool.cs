885cc7096a573f43f26aa10ec3846c787ee491ed
Unknown <arthur.delamare1@viacesi.fr>
2018-11-26 11:12:38 +0100

    [ADD] ObjectPool class

---
+++ Lightweight/ObjectPool/ObjectPool.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Concurrent;
+
+namespace Lightweight.ObjectPool
+{
+    public class ObjectPool<T> where T : new()
+    {
+        private readonly ConcurrentBag<T> _items = new ConcurrentBag<T>();
+        private int _counter = 0;
+        private int _MAX = 15;
+
+        public ObjectPool() { }
+
+        public ObjectPool(int size)
+        {
+            this._MAX = size;
+        }
+
+        public T Get()
+        {
+            T item;
+            if (_items.TryTake(out item))
+            {
+                this._counter--;
+            }
+            else
+            {
+                item = new T();
+                _items.Add(item);
+                this._counter++;
+            }
+            return item;
+        }
+
+        public void Release(T item)
+        {
+            if (this._counter < this._MAX)
+            {
+                this._items.Add(item);
+                this._counter++;
+            }
+        }
+    }
+}
